b5ae39becd7d3d539270052416756df83a89b8bf
Manuela Malasaña <Xibanya@users.noreply.github.com>
2019-11-30 20:12:44 -0600

    Add files via upload

---
+++ docs/CGIncludes/Data/link_definitions.js
@@ -0,0 +1,56 @@
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() 
+{
+    if (this.readyState == 4 && this.status == 200) 
+    {
+        var definitions = JSON.parse(this.responseText);
+        var pageName = location.href.split("/").slice(-1);
+        console.log("page name is " + pageName)
+        definitions.forEach(function(shaderField)
+        {
+            var page = "https://xibanya.github.io/ShaderTutorials/CGIncludes/" + shaderField.Include + ".html";
+            var page = shaderField.Include + ".html";
+            var linkString = page + "#" + shaderField.Name;
+            var newTag = "<a href=\"" + linkString + "\">" + shaderField.Field + "</a>";
+            findAndReplace(shaderField.Field, newTag, document.getElementById("test_body"));
+        });
+    }
+};
+xmlhttp.open("GET", "https://xibanya.github.io/ShaderTutorials/CGIncludes/Data/Definitions.json", true);
+xmlhttp.send();
+
+//adapted from https://j11y.io/snippets/find-and-replace-text-with-javascript/
+function findAndReplace(searchText, replacement, searchNode) {
+    if (!searchText || typeof replacement === 'undefined') {
+        // Throw error here if you want...
+        return;
+    }
+    var regex = typeof searchText === 'string' ?
+                new RegExp(`\\b${searchText}\\b`, 'gi') : searchText,
+        childNodes = (searchNode || document.body).childNodes,
+        cnLength = childNodes.length,
+        excludes = 'html,head,style,title,link,meta,script,object,iframe';
+    while (cnLength--) {
+        var currentNode = childNodes[cnLength];
+        if (currentNode.nodeType === 1 &&
+            (excludes + ',').indexOf(currentNode.nodeName.toLowerCase() + ',') === -1) {
+            arguments.callee(searchText, replacement, currentNode);
+        }
+        if (currentNode.nodeType !== 3 || !regex.test(currentNode.data) ) {
+            continue;
+        }
+        var parent = currentNode.parentNode,
+            frag = (function(){
+                var html = currentNode.data.replace(regex, replacement),
+                    wrap = document.createElement('div'),
+                    frag = document.createDocumentFragment();
+                wrap.innerHTML = html;
+                while (wrap.firstChild) {
+                    frag.appendChild(wrap.firstChild);
+                }
+                return frag;
+            })();
+        parent.insertBefore(frag, currentNode);
+        parent.removeChild(currentNode);
+    }
+}
