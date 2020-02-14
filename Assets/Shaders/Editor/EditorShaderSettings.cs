b10cd4856e2a127676970337361d9adeec5bccd0
Manuela Malasaña <Xibanya@users.noreply.github.com>
2020-02-13 22:29:09 -0600

    added EditorShaderSettings.cs

---
+++ Assets/Shaders/Editor/EditorShaderSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace Xibanya
+{
+    [InitializeOnLoad]
+    public static class EditorShaderSettings
+    {
+        static EditorShaderSettings()
+        {
+            ShaderGlobals.SetDefaults();
+        }
+    }
+}
